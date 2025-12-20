edee5050d8ea23f27a2b8b7fcfd4149e5a956bb5
Cody Wright <acodywright@gmail.com>
2025-12-19 18:09:54 -0800

    chore: delete outdated tests

--- modules/fts/tests/naming/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Integration tests for smart_template
-//!
-//! These tests use real multitrack setups to verify the mapping system works correctly
-//! with actual track naming conventions.
-
-pub mod marc_martel_dont_stop_me_now;
-pub mod ext_state_storage;
--- modules/fts/tests/template/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Template tests module
-
-mod default_tracklist_print;
-mod template_reorganization;
-
-pub use default_tracklist_print::*;
-pub use template_reorganization::*;
