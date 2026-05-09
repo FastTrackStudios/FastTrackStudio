3c529a0e48fe10d9f1d44ada92b220b3e6504ae0
Cody Wright <acodywright@gmail.com>
2026-05-09 13:46:05 -0700

    Shorten dynamic template create action labels

--- crates/dynamic-template-proto/src/actions.rs
+++ crates/dynamic-template-proto/src/actions.rs
@@ -38,205 +38,205 @@ actions_proto::define_actions! {
             group: "Dev",
         }
         CREATE_NEW_DRUMS = "create_new_drums" {
-            name: "Create New Drums",
+            name: "Drums",
             description: "Create a new drums template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_DRUM_KIT = "create_new_drum_kit" {
-            name: "Create New Drum Kit",
+            name: "Drum Kit",
             description: "Create a new drum kit template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_ELECTRONIC_KIT = "create_new_electronic_kit" {
-            name: "Create New Electronic Kit",
+            name: "Electronic Kit",
             description: "Create a new electronic kit template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_PERCUSSION = "create_new_percussion" {
-            name: "Create New Percussion",
+            name: "Percussion",
             description: "Create a new percussion template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_BASS = "create_new_bass" {
-            name: "Create New Bass",
+            name: "Bass",
             description: "Create a new bass template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_BASS_GUITAR = "create_new_bass_guitar" {
-            name: "Create New Bass Guitar",
+            name: "Bass Guitar",
             description: "Create a new bass guitar template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_BASS_SYNTH = "create_new_bass_synth" {
-            name: "Create New Bass Synth",
+            name: "Bass Synth",
             description: "Create a new bass synth template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_UPRIGHT_BASS = "create_new_upright_bass" {
-            name: "Create New Upright Bass",
+            name: "Upright Bass",
             description: "Create a new upright bass template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_GUITARS = "create_new_guitars" {
-            name: "Create New Guitars",
+            name: "Guitars",
             description: "Create a new guitars template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_ELECTRIC_GUITAR = "create_new_electric_guitar" {
-            name: "Create New Electric Guitar",
+            name: "Electric Guitar",
             description: "Create a new electric guitar template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_ACOUSTIC_GUITAR = "create_new_acoustic_guitar" {
-            name: "Create New Acoustic Guitar",
+            name: "Acoustic Guitar",
             description: "Create a new acoustic guitar template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_KEYS = "create_new_keys" {
-            name: "Create New Keys",
+            name: "Keys",
             description: "Create a new keys template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_PIANO = "create_new_piano" {
-            name: "Create New Piano",
+            name: "Piano",
             description: "Create a new piano template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_ORGAN = "create_new_organ" {
-            name: "Create New Organ",
+            name: "Organ",
             description: "Create a new organ template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_ELECTRIC_KEYS = "create_new_electric_keys" {
-            name: "Create New Electric Keys",
+            name: "Electric Keys",
             description: "Create a new electric keys template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_SYNTHS = "create_new_synths" {
-            name: "Create New Synths",
+            name: "Synths",
             description: "Create a new synths template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_SYNTH_LEAD = "create_new_synth_lead" {
-            name: "Create New Synth Lead",
+            name: "Synth Lead",
             description: "Create a new synth lead template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_SYNTH_PAD = "create_new_synth_pad" {
-            name: "Create New Synth Pad",
+            name: "Synth Pad",
             description: "Create a new synth pad template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_SYNTH_ARP = "create_new_synth_arp" {
-            name: "Create New Synth Arp",
+            name: "Synth Arp",
             description: "Create a new synth arp template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_HORNS = "create_new_horns" {
-            name: "Create New Horns",
+            name: "Horns",
             description: "Create a new horns template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_TRUMPET = "create_new_trumpet" {
-            name: "Create New Trumpet",
+            name: "Trumpet",
             description: "Create a new trumpet template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_TROMBONE = "create_new_trombone" {
-            name: "Create New Trombone",
+            name: "Trombone",
             description: "Create a new trombone template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_SAXOPHONE = "create_new_saxophone" {
-            name: "Create New Saxophone",
+            name: "Saxophone",
             description: "Create a new saxophone template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_HARMONICA = "create_new_harmonica" {
-            name: "Create New Harmonica",
+            name: "Harmonica",
             description: "Create a new harmonica template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_STRINGS = "create_new_strings" {
-            name: "Create New Strings",
+            name: "Strings",
             description: "Create a new strings template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_VOCALS = "create_new_vocals" {
-            name: "Create New Vocals",
+            name: "Vocals",
             description: "Create a new vocals template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_LEAD_VOCALS = "create_new_lead_vocals" {
-            name: "Create New Lead Vocals",
+            name: "Lead Vocals",
             description: "Create a new lead vocals template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_BACKGROUND_VOCALS = "create_new_background_vocals" {
-            name: "Create New Background Vocals",
+            name: "Background Vocals",
             description: "Create a new background vocals template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_CHOIR = "create_new_choir" {
-            name: "Create New Choir",
+            name: "Choir",
             description: "Create a new choir template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_ORCHESTRA = "create_new_orchestra" {
-            name: "Create New Orchestra",
+            name: "Orchestra",
             description: "Create a new orchestra template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_SFX = "create_new_sfx" {
-            name: "Create New SFX",
+            name: "SFX",
             description: "Create a new SFX template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_GUIDE = "create_new_guide" {
-            name: "Create New Guide",
+            name: "Guide",
             description: "Create a new guide template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_REFERENCE = "create_new_reference" {
-            name: "Create New Reference",
+            name: "Reference",
             description: "Create a new reference template group",
             category: General,
             group: "Create",
         }
         CREATE_NEW_STEM_SPLIT = "create_new_stem_split" {
-            name: "Create New Stem Split",
+            name: "Stem Split",
             description: "Create a new stem split template group",
             category: General,
             group: "Create",
